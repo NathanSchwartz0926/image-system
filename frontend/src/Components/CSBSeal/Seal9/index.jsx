d9bbe88114af8d3ed35f31da9412848960b537e8
unknown <nathanschwartz0926@gmail.com>
2025-01-21 06:52:59 -0600

    Change csb seal 9

--- frontend/src/Components/CSBSeal/Seal9/index.jsx
+++ frontend/src/Components/CSBSeal/Seal9/index.jsx
@@ -11,13 +11,13 @@ const Seal9 = ({ branchInfo }) => {
     <div className="CSB-seal9-stamp-container">
       <div className="CSB-seal9-stamp-box" style = {{letterSpacing:"-0.6px"}}>
         <div style = {{display:"flex"}}>
-          <p>Duplicate DD No: </p>
-          <div style = {{flexGrow: "1", border: "None", borderBottom: "2.5px dotted black"}}></div>
+          <p>Duplicate DD No :</p>
+          <div className = "CSB-seal9-one" style = {{flexGrow: "1", border: "None", letterSpacing:"normal"}}>..........................</div>
           <p>issued in lieu</p>
         </div>
         <div style = {{display:"flex"}}>
-          <p>of Original DD No: </p>
-          <div style = {{flexGrow: "1", border: "None", borderBottom: "2.5px dotted black"}}></div>
+          <p>of Original DD No :</p>
+          <div className = "CSB-seal9-one" style = {{flexGrow: "1", border: "None", letterSpacing:"normal"}}>........................</div>
           <p>reported lost.</p>
         </div>
       </div>
